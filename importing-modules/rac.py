276d3193bea20a3b8d66522ac3b0a09f8477a1c2
Richard Conway <richard@rconway.co.uk>
2019-06-08 11:46:12 +0100

    Tweaked rac.py to use main method.

--- importing-modules/rac.py
+++ importing-modules/rac.py
@@ -1,21 +1,25 @@
-
-print("hello")
-
-from fred import world
-world()
-
-import fred
-fred.world()
-
-from larry.bob import universe
-universe()
-
-import larry.bob as bob
-bob.universe()
-
-import larry.bob
-larry.bob.universe()
-
 # relies upon __all__ being set in the __init__.py
 from larry import *
-bob.universe()
+
+def main():
+    print("hello")
+
+    from fred import world
+    world()
+
+    import fred
+    fred.world()
+
+    from larry.bob import universe
+    universe()
+
+    import larry.bob as bob
+    bob.universe()
+
+    import larry.bob
+    larry.bob.universe()
+
+    bob.universe()
+
+if __name__ == "__main__":
+    main()
